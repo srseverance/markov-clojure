b74f3c9f94ac0e20749b20c909000e77eb2d3f4a
Shannon Severance <srs@s53.me>
2015-03-31 22:48:25 -0700

    build function complete

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject markov "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The BSD 2-Clause License"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot markov.core
   :target-path "target/%s"
--- src/markov/core.clj
+++ src/markov/core.clj
@@ -1,7 +1,28 @@
 (ns markov.core
+  (:require [clojure.java.io :as io])
   (:gen-class))
+
+(def gettysburg-address "/Volumes/Macintosh HD/Users/srseverance/Learning/corpus/gettysburg-address.txt")
+
+(def sicp "/Volumes/Macintosh HD/Users/srseverance/Downloads/sicp.texi")
+
+(def number-of-prefix 2)
+
+(defn add-state [state-map prefix suffix]
+  (update-in state-map [prefix] #(conj (or %1 []) %2) suffix))
+
+(defn build [file-name]
+  (with-open [source (io/reader file-name)]
+    (->> source
+         line-seq
+         (mapcat #(clojure.string/split % #"\s+"))
+         (cons :not-a-word)
+         (cons :not-a-word)
+         (filter #(not= ""%))
+         (partition (inc number-of-prefix) 1)
+         (reduce #(add-state %1 (butlast %2) (last %2)) {}))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (doseq [l gettysburg-address] (println l)))
