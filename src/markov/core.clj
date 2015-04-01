cb5a4b8c32da98138f35f2f58ed4beceb4d94bd4
Shannon Severance <srs@s53.me>
2015-04-01 05:37:54 -0700

    Done, with simple command line args

--- src/markov/core.clj
+++ src/markov/core.clj
@@ -3,26 +3,41 @@
   (:gen-class))
 
 (def gettysburg-address "/Volumes/Macintosh HD/Users/srseverance/Learning/corpus/gettysburg-address.txt")
-
+(def simple-txt "/Volumes/Macintosh HD/Users/srseverance/Learning/Clojure/markov/resources/simple.txt")
 (def sicp "/Volumes/Macintosh HD/Users/srseverance/Downloads/sicp.texi")
+(def the-common-law "/Volumes/Macintosh HD/Users/srseverance/Downloads/the-common-law.txt")
+(def flatland "/Volumes/Macintosh HD/Users/srseverance/Downloads/flatland.txt")
 
 (def number-of-prefix 2)
+(def starting-prefix (repeat number-of-prefix :not-a-word))
 
 (defn add-state [state-map prefix suffix]
   (update-in state-map [prefix] #(conj (or %1 []) %2) suffix))
 
 (defn build [file-name]
-  (with-open [source (io/reader file-name)]
-    (->> source
-         line-seq
-         (mapcat #(clojure.string/split % #"\s+"))
-         (cons :not-a-word)
-         (cons :not-a-word)
-         (filter #(not= ""%))
-         (partition (inc number-of-prefix) 1)
-         (reduce #(add-state %1 (butlast %2) (last %2)) {}))))
+  (with-open [r (io/reader file-name)]
+    (->> r
+       line-seq
+       (mapcat #(clojure.string/split % #"\s+"))
+       (filter #(not= "" %))
+       (concat starting-prefix)
+       (partition (inc number-of-prefix) 1)
+       (reduce #(add-state %1 (butlast %2) (last %2)) {}))))
+
+(defn generate [state-map n]
+  (letfn [(do-the-needful [prefix n]
+            (lazy-seq
+             (let [suffix (rand-nth (state-map prefix))]
+               (if (or (zero? n) (nil? suffix))
+                 nil
+                 (cons suffix
+                       (do-the-needful (-> prefix pop (conj suffix))
+                                 (dec n)))))))]
+    (doseq [s (interpose " " (do-the-needful (into clojure.lang.PersistentQueue/EMPTY starting-prefix) n))]
+      (print s))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (doseq [l gettysburg-address] (println l)))
+  (let [[file-name n-str] args]
+    (generate (build file-name) (Integer/parseInt n-str))))
